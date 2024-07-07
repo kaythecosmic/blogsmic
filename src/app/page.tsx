327ba4fc24c0a8f842a5d30794f955c039656ddf
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-07-07 21:52:07 +0530

    filler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,54 +7,74 @@ import Spinner from "@/components/ui/spinner";
 import Link from "next/link";
 import useSWR, { mutate } from "swr";
 import fetcher from "@/lib/fetcher";
+import axios from "axios";
 
 export default function Home() {
 
-  const [blogs, setBlogs] = useState<typeBlog[]>()
-  const { data, error, isLoading } = useSWR<typeBlog[]>(`/api/blogs`, fetcher);
-  useEffect(() => {
-    if (data && !isLoading) {
-      // data.reverse();
-      setBlogs(data); 
-    } else if (error) {
-      console.log("Still Fetching");
+    const [blogs, setBlogs] = useState<typeBlog[]>()
+
+    const fetch = async (url: string) => {
+        try {
+            const res = await axios.get(url).then(res => res.data)
+            return res
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const { data, error, isLoading, mutate } = useSWR<typeBlog[]>(`/api/blogs`, fetch,
+        {
+            revalidateOnFocus: true,
+            revalidateIfStale: true,
+            revalidateOnReconnect: true
+        });
+    mutate()
+
+    console.log("Hello 1");
+    console.log(blogs);
+    
+    useEffect(() => {
+        if (data && !isLoading) {
+            setBlogs(data);
+        } else if (error) {
+            console.log("Still Fetching");
+        }
+    },[data]);
+
+    if (isLoading) {
+        return (
+            <MaxWidthWrapper className="pt-4">
+                <h5 className="flex justify-center items-center text-center p-3">
+                    <Spinner />
+                </h5>
+            </MaxWidthWrapper>
+        );
     }
-  }, [data]);
 
-  if (isLoading) {
+    if (error) {
+        return (
+            <MaxWidthWrapper className=" pt-4">
+                <h5 className=" bg-muted text-center p-3">
+                    Could not fetch blogs. Please reload.
+                </h5>
+            </MaxWidthWrapper>
+        );
+    }
+
     return (
-      <MaxWidthWrapper className="pt-4">
-        <h5 className="flex justify-center items-center text-center p-3">
-          <Spinner />
-        </h5>
-      </MaxWidthWrapper>
+        <MaxWidthWrapper className="my-4">
+            {blogs && blogs.length > 0 ? blogs.map((blog) => (
+                <Blog
+                    key={blog.id}
+                    id={blog.id}
+                    title={blog.title}
+                    slug={blog.slug}
+                    content={blog.content}
+                    date={new Date(blog.date)}
+                    readTime={blog.readTime}
+                />
+            )) : <div className="text-2xl text-center p-4 font-bold">
+                Be the first one to <Link href="/scratch" className="text-emerald-500 underline hover:text-emerald-700">post a blog!</Link> </div>}
+        </MaxWidthWrapper>
     );
-  }
-
-  if (error) {
-    return (
-      <MaxWidthWrapper className=" pt-4">
-        <h5 className=" bg-muted text-center p-3">
-          Could not fetch blogs. Please reload.
-        </h5>
-      </MaxWidthWrapper>
-    );
-  }
-
-  return (
-    <MaxWidthWrapper className="my-4">
-      {data && data.length > 0 ? data.map((blog) => (
-        <Blog
-          key={blog.id}
-          id={blog.id}
-          title={blog.title}
-          slug={blog.slug}
-          content={blog.content}
-          date={new Date(blog.date)}
-          readTime={blog.readTime}
-        />
-      )) : <div className="text-2xl text-center p-4 font-bold">
-        Be the first one to <Link href="/scratch" className="text-emerald-500 underline hover:text-emerald-700">post a blog!</Link> </div>}
-    </MaxWidthWrapper>
-  );
 }
